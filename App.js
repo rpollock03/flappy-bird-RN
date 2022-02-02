8c8f43dacbf501b4f4dcce7a3b2df70817403273
Robert Pollock <robertpollock@Roberts-MacBook-Air.local>
2022-02-02 09:06:13 +0000

    add scoring

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 
+import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar"
 import React, {useState, useEffect} from "react"
-import { StyleSheet, View, Dimensions, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, View, Text, Dimensions, TouchableWithoutFeedback } from 'react-native'
 import Bird from './components/Bird'
 import Obstacles from "./components/Obstacles"
 
 
 export default function App() {
+
+  const [isGameOver, setIsGameOver] = useState(false)
+  const [score, setScore]=useState(0)
+
 
   const screenWidth = Dimensions.get("screen").width
   const screenHeight= Dimensions.get("screen").height
@@ -14,7 +19,7 @@ export default function App() {
   const birdLeft = screenWidth / 2
   const [birdBottom, setBirdBottom] = useState(screenHeight / 2)
   const gravity = 3
-  let gameTimerId
+  let gameTimerId, obstaclesLeftTimerId, obstaclesLeftTimerIdTwo
 
 
   //randomise obstacles heights 
@@ -38,7 +43,6 @@ export default function App() {
   //start obstacles scrolling
   const[obstaclesLeft, setObstaclesLeft] = useState(screenWidth) //so starts off the page
   
-  let obstaclesLeftTimerId
   const obstacleWidth = 60
   const obstacleHeight = 300
   const gap = 200
@@ -55,14 +59,13 @@ export default function App() {
     else {
       setObstaclesLeft(screenWidth)
       setObstaclesNegHeight(- Math.random()*100)
+      setScore(score => score+1)
     }
   },[obstaclesLeft])
 
   //start second obstacle
 
   const[obstaclesLeftTwo, setObstaclesLeftTwo] = useState(screenWidth + screenWidth/2 +30) 
-  
-  let obstaclesLeftTimerIdTwo
 
   useEffect(()=>{
     if(obstaclesLeftTwo>-obstacleWidth){  //obstaclewidth so object disapeares
@@ -76,9 +79,9 @@ export default function App() {
     else {
       setObstaclesLeftTwo(screenWidth)
       setObstaclesNegHeightTwo(- Math.random()*100)
+      setScore(score => score+1)
     }
   },[obstaclesLeftTwo])
-
 
 
   //check for collisons
@@ -98,26 +101,31 @@ export default function App() {
     {
       console.log("game over")
       gameOver()
-      setIsGameOver(true)
+      
     }
     
-  },[])
+  })
 
- 
+
 
   const gameOver = () => {
     clearInterval(gameTimerId)
     clearInterval(obstaclesLeftTimerId)
-    clearInterval(obstaclesTimerIdTwo)
+    clearInterval(obstaclesLeftTimerIdTwo)
+    setIsGameOver(true)
   }
 
   const jump = () => {
-    
+    if(!isGameOver && (birdBottom < screenHeight)){
+      setBirdBottom(birdBottom => birdBottom +50)
+      console.log("jumped!")
+    }
   }
 
   return (
   <TouchableWithoutFeedback onPress={jump}> 
   <View style={styles.container}>
+    {isGameOver && <Text>{score}</Text>}
       <Bird 
         birdBottom={birdBottom}
         birdLeft={birdLeft}
